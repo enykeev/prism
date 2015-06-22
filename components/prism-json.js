1c4b29aa2d3b0ce3e0a793e51e17ffaa054ccf1a
Kirill Izotov <enykeev@stackstorm.com>
2015-06-22 16:29:06 +0600

    JSON string special chartacters fix

--- components/prism-json.js
+++ components/prism-json.js
@@ -1,6 +1,6 @@
 Prism.languages.json = {
     'property': /"(\b|\B)[\w-]+"(?=\s*:)/ig,
-    'string': /"(?!:)(\\?[^'"])*?"(?!:)/g,
+    'string': /"([^"\\]*|\\["\\bfnrt/]|\\u[0-9a-f]{4})*"/g,
     'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/g,
     'function': {
 		pattern: /[a-z0-9_]+\(/ig,
